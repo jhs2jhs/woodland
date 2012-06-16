c71787b3101de451203be007931f087bb084a091
jianhuashao <jianhua.shao1986@gmail.com>
2012-06-16 15:46:44 +0100

    init django web server

---
+++ django/woodland/woodland/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'woodland.views.home', name='home'),
+    # url(r'^woodland/', include('woodland.foo.urls')),
+    url(r'^main/', include('main_code.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
