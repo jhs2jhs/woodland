1c1f19593fa228880d3270279fc309d8bbf92ebb
Jianhua Shao <jianhua.shao1986@gmail.com>
2012-07-24 15:43:23 +0100

    test for file upload

--- django/woodland/main_code/urls.py
+++ django/woodland/main_code/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^hello', 'main_code.views.hello'),
+    url(r'^test_file_view', 'main_code.views.test_file'),
+    url(r'^test_file_upload', 'main_code.views.test_file_upload'),
 )
--- django/woodland/main_code/views.py
+++ django/woodland/main_code/views.py
@@ -1,6 +1,36 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def hello(request):
     print request
     return HttpResponse("Hello, main woodland")
+
+from django import forms
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
+
+def test_file(request):
+    #return HttpResponse('hello, file upload test')
+    context = RequestContext(request)
+    return render_to_response('file_upload.html', context)
+
+def test_file_upload(request):
+    fc = ''
+    if request.method == 'POST':
+        files = request.FILES
+        if files.has_key('myfile'):
+            fc = files['myfile']
+            f = open('./'+fc.name, 'w')
+            if fc.multiple_chunks:
+                for c in fc.chunks():
+                    f.write(c)
+            else:
+                f.write(fc.read())
+            f.close()
+    print 'hello'
+    print fc
+    #print request.FILES
+    return HttpResponse("hello, file")
