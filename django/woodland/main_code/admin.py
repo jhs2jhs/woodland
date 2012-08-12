b441e3ab586407d9b0196be8d244601dbd52545e
Jianhua Shao <jianhua.shao1986@gmail.com>
2012-08-12 16:10:35 +0100

    add admin page in

---
+++ django/woodland/main_code/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from main_code.models import *
+
+admin.site.register(Tag)
+admin.site.register(Ptjt)
+admin.site.register(UploadFile)
+admin.site.register(Comment)
